0a27a3b9b3f66954a7f8d251db539444ec875d76
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-03-11 10:33:44 -0300

    refactor:Refatorando componentes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
-import Search from './Search';
+import Home from '../pages/Home';
 
 class Routes extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route path="/" component={ Search } />
+          <Route exact path="/" component={ Home } />
         </Switch>
       </div>
     );
